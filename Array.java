ce2fa3370bf22218adcef96ecb40ea93c7b0ae0a
SAMIKSHA CHIBADE <samiksha.chibade23@mmit.edu.in>
2026-08-20 22:17:17 +0530

    Add resultArray method to split input array
    Implement resultArray method to separate elements into two arrays based on comparison.

---
+++ Array.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int[] resultArray(int[] nums) {
+
+        int n = nums.length;
+
+        int[] arr1 = new int[n];
+        int[] arr2 = new int[n];
+
+        int size1 = 0;
+        int size2 = 0;
+
+        // First operation
+        arr1[size1++] = nums[0];
+
+        // Second operation
+        arr2[size2++] = nums[1];
+
+        // Remaining elements
+        for (int i = 2; i < n; i++) {
+
+            if (arr1[size1 - 1] > arr2[size2 - 1]) {
+                arr1[size1++] = nums[i];
+            } else {
+                arr2[size2++] = nums[i];
+            }
+        }
+
+        // Create result
+        int[] result = new int[n];
+
+        int index = 0;
+
+        // Copy arr1
+        for (int i = 0; i < size1; i++) {
+            result[index++] = arr1[i];
+        }
+
+        // Copy arr2
+        for (int i = 0; i < size2; i++) {
+            result[index++] = arr2[i];
+        }
+
+        return result;
+    }
+}
